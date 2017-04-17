07ff3c6c79f495ffb5fd5275dce5d6c3b17b284a
Kuldeep Singh <kdmalviyan@gmail.com>
2017-04-17 23:01:06 +0530

    Initial commit

---
+++ src/main/java/com/kd/example/spring/exceptions/EmployeeNotFoundException.java
@@ -0,0 +1,17 @@
+package com.kd.example.spring.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Employee Not Found") // 404
+public class EmployeeNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1536473620191887324L;
+
+	public EmployeeNotFoundException(int id) {
+		super("EmployeeNotFoundException with id=" + id);
+	}
+}
---
+++ src/main/java/com/kd/example/spring/resolver/MySimpleMappingExceptionResolver.java
@@ -0,0 +1,7 @@
+package com.kd.example.spring.resolver;
+
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+public class MySimpleMappingExceptionResolver extends SimpleMappingExceptionResolver {
+
+}
